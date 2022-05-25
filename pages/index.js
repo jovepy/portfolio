020e6c9ecbab2347180b4fe2b1ea1e4dc2dab45e
Rodrigo Jovê <datajove@gmail.com>
2022-05-25 13:03:07 -0300

    confirmar em main

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import { useState} from 'react';
-import Head from 'next/head';
+//import Head from 'next/head';
 import Router from 'next/router';
 
 
@@ -7,39 +7,13 @@ import Router from 'next/router';
 function Home(){
     return(
         <div >
-            
-                <Head>
-                <title>Jovepy</title>
-                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-                <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous"></link>
-        
-                </Head>
-            
-                <div class="container-fluid">
-                    <div class="col-sm-12 bg- p-3 border">
-                        <div class="row justify-content-center">
-                            <div class="d-grid gap-5 col-6 mx-auto px-auto">
-                                <button class="btn btn-primary-dark" type="button" onClick={() => Router.push('/jovepy')} >Jove.py</button>
-                                <button class="btn btn-primary-dark" type="button" onClick={() => Router.push('/jovepy')} >Caderno</button>
-                                <button class="btn btn-primary-dark" type="button" onClick={() => Router.push('/jovepy')} >Empreenda</button>
-                                <button class="btn btn-primary-dark" type="button" onClick={() => Router.push('/jovepy')} >Opere com Derivativos</button>
-                                <button class="btn btn-primary-dark" width = "400px" type="button" onClick={() => Router.push('/jovepy')} >Invista com FI's</button>
-                                
-                                <Contador />
-                                
-                                
-                            </div>
-                        </div>
-                </div>
-                <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
-                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-kjU+l4N0Yf4ZOJErLsIcvOU2qSb74wXpOhqTvwVx3OElZRweTnQ6d31fXEoRD1Jy" crossorigin="anonymous"></script>
-                <footer>
-                    <div class="text-bg-dark p-5">
-                        
-                    </div>
-                </footer>
+            <button class="btn btn-primary-dark" type="button" onClick={() => Router.push('/jovepy')} >Jove.py</button>
+            <button class="btn btn-primary-dark" type="button" onClick={() => Router.push('/jovepy')} >Caderno</button>
+            <button class="btn btn-primary-dark" type="button" onClick={() => Router.push('/jovepy')} >Empreenda</button>
+            <button class="btn btn-primary-dark" type="button" onClick={() => Router.push('/jovepy')} >Opere com Derivativos</button>
+            <button class="btn btn-primary-dark" width = "400px" type="button" onClick={() => Router.push('/jovepy')} >Invista com FI's</button>   
+            <Contador />
         </div>
-    </div>
     
     )
 }
