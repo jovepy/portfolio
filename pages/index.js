c49cf40cee21caa06ff60e23b698f9e72d9fa387
Rodrigo Jovê <datajove@gmail.com>
2022-05-24 21:37:05 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -14,18 +14,13 @@ function Home(){
                 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous"></link>
         
                 </Head>
-                
-                    <nav class="navbar navbar-dark bg-dark">
-                        <form class="container-fluid justify-content-start">
-                            <button class="mx-auto btn btn-outline-light" type="button" onClick={() => Router.push('/jovepy')} >Jove.py</button>
-                            <button class="mx-auto btn btn-outline-light" type="button" onClick={() => Router.push('/caderno')} >Caderno</button>
-                        </form>
-                    </nav>
-                
+            
                 <div class="container-fluid">
                     <div class="col-sm-12 bg- p-3 border">
                         <div class="row justify-content-center">
                             <div class="d-grid gap-5 col-6 mx-auto px-auto">
+                                <button class="btn btn-primary-dark" type="button" onClick={() => Router.push('/jovepy')} >Jove.py</button>
+                                <button class="btn btn-primary-dark" type="button" onClick={() => Router.push('/jovepy')} >Caderno</button>
                                 <button class="btn btn-primary-dark" type="button" onClick={() => Router.push('/jovepy')} >Empreenda</button>
                                 <button class="btn btn-primary-dark" type="button" onClick={() => Router.push('/jovepy')} >Opere com Derivativos</button>
                                 <button class="btn btn-primary-dark" width = "400px" type="button" onClick={() => Router.push('/jovepy')} >Invista com FI's</button>
