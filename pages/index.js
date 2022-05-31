98756c8aa75e997cd27e9ba1efcf64bcc90a8253
Rodrigo Jovê <datajove@gmail.com>
2022-05-30 21:27:41 -0300

    commit

--- pages/index.js
+++ pages/index.js
@@ -37,23 +37,20 @@ function Home(){
                           <div class="d-grid gap-4">
                               <div class='greeting'>
                                     <h1 ><Saudacao /> Seja bem-vindo ao meu ecossistema</h1>
-                                    <p> Aqui você será redirecionado para todos os meus projetos</p>
+                                    <p> Aqui você encontrará o caminho para todos os meus projetos</p>
                               </div>
                               <button class="btn btn-outline-light" type="button" onClick={() => Router.push('/jovepy')} >Jove.py</button>
-                              <button class="btn btn-outline-light" type="button" onClick={() => Router.push('/caderno')} >Caderno</button>
                               <button class="btn btn-outline-light" type="button" onClick={() => Router.push('https://jovepy.github.io/empreenda/')} >Empreenda</button>
-                              <button class="btn btn-outline-light" type="button" onClick={() => Router.push('/https://github.com/jovepy/Opere-com-derivativos')} >Opere com Derivativos</button>
-                              <button class="btn btn-outline-light" type="button" onClick={() => Router.push('/https://github.com/jovepy/Fundos_Investimento_B3')} >Invista com FI's</button>  
+                              <button class="btn btn-outline-light" type="button" onClick={() => Router.push('https://github.com/jovepy/Opere-com-derivativos')} >Opere com Derivativos</button>
+                              <button class="btn btn-outline-light" type="button" onClick={() => Router.push('https://github.com/jovepy/Fundos_Investimento_B3')} >Invista com FI's</button>  
+                              <div class="row"></div>
                           </div>
-  
-                          
                       </div>
-                      
                   </div>
                   <div class="col-6 col-md-4"></div>
-                  
               </div>
         </div>
+        
         </div>
 
       )
--- pages/jovepy.js
+++ pages/jovepy.js
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import React from 'react'
+import Router from 'next/router';
 
 const myLoader = ({ src, width, quality }) => {
   return `./${src}?w=${width}&q=${quality || 75}`
@@ -7,15 +8,55 @@ const myLoader = ({ src, width, quality }) => {
 
 function jovepy() {
     return (
-      <div>
-      <div class="bg"></div>
-      <div class="star-field">
-      <div class="layer"></div>
-      <div class="layer"></div>
-      <div class="layer"></div>
+    <div class="container-fluid" >
+    <div class="bg"></div>
+    <div class="star-field">
+    <div class="layer"></div>
+    <div class="layer"></div>
+    <div class="layer"></div>
+    <div class="d-grid gap-3">
+    <div class="row">
+                  <div class="col-6 col-md-4"></div>
+                  <div class="col-6 col-md-4">
+                      <div class="position-absolute top-50 start-50 translate-middle">  
+                          <div class="d-grid gap-4"></div>
+                            <div class="d-grid gap-4">
+                            <button type="button" class="btn-close btn-close-white" type="button" onClick={() => Router.push('/')}></button>
+                              <div class="card mb-3">
+                                <div class="row g-0">                                    
+                                  <div class="col-md-4">
+                                  <Image loader={myLoader} src="images/perfilalt.png" alt="Rosto de Jovepy" width={300} height={300} class="profile"/>
+                                  </div>
+                                  <div class="col-md-8">
+                                    <div class="card-body">
+                                      <h5 class="fw-bold">With open ('jove.py', 'r') as me:</h5>
+                                      <p class="card-text">print(me.read())</p>
+                                      <p class="font-monospace"> >>>26 anos, Graduando em Economia e Desenvolvedor Python</p>
+                                      <p class="card-text"><small class="text-muted">atualizado em 30 de maio de 2022</small></p>
+                                    </div>
+                                    
+                                  </div>
+                                  
+                                </div>
+                                
+                              </div>
+                              <button class="btn btn-outline-light" type="button" onClick={() => Router.push('https://www.linkedin.com/in/rodrigojove')}>Linkedin</button>
+                              <button class="btn btn-outline-light" type="button" onClick={() => Router.push('https://api.whatsapp.com/send?phone=83998775808&text=Olá!!')}>Envie-me uma mensagem</button>
+                              <div class="row"></div>
+                            </div>
+                            
+                          </div>
+  
+                          
+                      </div>
+                      
+                  </div>
+                  <div class="col-6 col-md-4"></div>
+                  
+              </div>
+        </div>
+        </div>
 
-      </div>
-      </div>
     )
 
   }
