03b0b8877b03617ca78b1029ec05fa3d6a72aeb8
Rodrigo Jovê <datajove@gmail.com>
2022-05-31 21:44:22 -0300

    finish

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,7 @@ function Saudacao(){
 
 function Home(){
     return (
-        <div class="mx-auto">
+        <div class="container-fluid">
         <div class="bg"></div>
         <div class="star-field">
         <div class="layer"></div>
@@ -29,7 +29,7 @@ function Home(){
         <div class="row">
                   <div class="col-6 col-md-4"></div>
                   <div class="col-6 col-md-4">
-                      <div class="position-absolute top-50 start-50 translate-middle">
+                      <div class="row align-items-center">
                           <div class="d-grid gap-4">
                               <div class='greeting'>
                                     <h1 ><Saudacao /> Seja bem-vindo ao meu portfólio</h1>
